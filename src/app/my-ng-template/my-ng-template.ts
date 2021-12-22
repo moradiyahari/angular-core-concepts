54a8ebef7cd2460a7a347665c58109f183505691
moradiyahari <moradiyahari@ymail.com>
2021-12-22 13:00:00 +0530

    3 files modified, 1 deleted

--- src/app/my-ng-template/my-ng-template.ts
+++ src/app/my-ng-template/my-ng-template.ts
@@ -1,10 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-ng-template',
   templateUrl: './my-ng-template.html',
-  styleUrls: [ './my-ng-template.css' ]
+  styleUrls: ['./my-ng-template.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class MyNgTemplateComponent {
+  items = [
+    { name: 'A', id: 1 },
+    { name: 'B', id: 2 },
+    { name: 'C', id: 3 },
+  ];
+  constructor() {}
+  trackById(item) {
+    return item.id;
+  }
 }
