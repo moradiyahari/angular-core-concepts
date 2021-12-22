57a3a02b8f7568cb09b31be5b3a518c5b5d06bbb
moradiyahari <moradiyahari@ymail.com>
2021-12-22 15:25:40 +0530

    3 files modified

--- src/app/view-child/demo.component.ts
+++ src/app/view-child/demo.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'demo',
-  template: `<h1>Hello, {{name}}!</h1>`,
+  template: `<h4>Hello, {{name}}!</h4>`,
   styles: [`h1 { font-family: Lato; }`],
 })
 export class DemoComponent {
--- src/app/view-children/view-children.ts
+++ src/app/view-children/view-children.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, AfterViewInit, QueryList } from '@angular/core';
+import { DemoComponent } from '../view-child/demo.component';
 
 @Component({
   selector: 'view-children',
   templateUrl: './view-children.html',
 })
 export class ViewChildrenComponent {
+  @ViewChildren(DemoComponent) demos: QueryList<any>;
+
   constructor() {}
+
+  ngAfterViewInit() {
+     this.demos.forEach(demo => console.log(demo.name));
+  }
 }
