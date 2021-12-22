cb15f33de0624f37dd2450a76669e80f8b613e79
moradiyahari <moradiyahari@ymail.com>
2021-12-22 15:19:36 +0530

    2 files modified

--- src/app/view-child/view-child.ts
+++ src/app/view-child/view-child.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { DemoComponent } from './demo.component';
 
 @Component({
   selector: 'view-child',
   templateUrl: './view-child.html',
 })
 export class ViewChildComponent {
+  name = 'Harikrushna';
+
+  //Example to access child component
+  @ViewChild(DemoComponent, { static: false }) hello: DemoComponent;
+
+  //Example to access child element
+  @ViewChild('myPRef', { static: false }) myPRef: ElementRef;
+
   constructor() {}
+
+  ngAfterViewInit() {
+    console.log('Hello ', 'j' + this.hello.name, 3);
+    console.log(this.myPRef.nativeElement.innerHTML);
+    this.myPRef.nativeElement.innerHTML = 'DOM updated succesfully!!!';
+  }
 }
