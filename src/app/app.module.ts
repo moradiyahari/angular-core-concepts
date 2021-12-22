a1a18a03068c848cedeea8bcfef6b883102f3394
moradiyahari <moradiyahari@ymail.com>
2021-12-22 13:25:31 +0530

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MyNgTemplateComponent } from './my-ng-template/my-ng-template';
+import { MyNgTemplateOutletComponent } from './my-ng-template-outlet/my-ng-template-outlet';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MyNgTemplateComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    MyNgTemplateComponent,
+    MyNgTemplateOutletComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/my-ng-template-outlet/my-ng-template-outlet.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-ng-template-outlet',
+  templateUrl: './my-ng-template-outlet.html',
+})
+export class MyNgTemplateOutletComponent {
+  constructor() {}
+}
