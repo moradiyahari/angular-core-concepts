3fa4ebe07f0b9c84a77ab53dac4db53c6eb498f1
moradiyahari <moradiyahari@ymail.com>
2021-12-22 13:38:35 +0530

    2 files modified

--- src/app/my-ng-container/my-ng-container.ts
+++ src/app/my-ng-container/my-ng-container.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-ng-container',
   templateUrl: './my-ng-container.html',
+  styleUrls: ['./../my-ng-template/my-ng-template.css'],
 })
 export class MyNgContainerComponent {
+  items = [
+    { name: 'A', id: 1 },
+    { name: 'B', id: 2 },
+    { name: 'C', id: 3 },
+  ];
   constructor() {}
+  trackById(item) {
+    return item.id;
+  }
 }
