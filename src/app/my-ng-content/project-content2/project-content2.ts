e8001354b835e49e64d5a55e6ea306ea04a5b97e
moradiyahari <moradiyahari@ymail.com>
2021-12-22 16:02:00 +0530

    1 file modified

--- src/app/my-ng-content/project-content2/project-content2.ts
+++ src/app/my-ng-content/project-content2/project-content2.ts
@@ -9,6 +9,68 @@ export class ProjectContent2Component {
   @ContentChild('header') projectContentHeader: ElementRef;
 
   constructor(private renderor: Renderer2) {
-    console.log('ProjectContent2', 'constructor', this.projectContentHeader);
+    //returns undfined
+    console.log(
+      'ProjectContent2->',
+      'constructor->',
+      this.projectContentHeader
+    );
+  }
+
+  ngOnChanges() {
+    //first time returns undfined
+    console.log(
+      'ProjectContent2->',
+      'ngOnChanges->',
+      this.projectContentHeader
+    );
+  }
+
+  ngOnInit() {
+    //returns undfined
+    console.log('ProjectContent2->', 'ngOnInit->', this.projectContentHeader);
+  }
+
+  ngDoCheck() {
+    //first time returns undfined
+    console.log('ProjectContent2->', 'ngDoCheck->', this.projectContentHeader);
+  }
+
+  ngAfterContentInit() {
+    //Header is available here
+    console.log(
+      'ProjectContent2->',
+      'ngAfterContentInit->',
+      this.projectContentHeader.nativeElement.innerHTML
+    );
+    //this.renderor.setStyle(this.projectContentHeader.nativeElement,"font-size","30px")
+    //this.projectContentHeader.nativeElement.innerHTML="<h3>Test Update</h3>"
+  }
+
+  ngAfterContentChecked() {
+    //Header is available here
+    console.log(
+      'ProjectContent2->',
+      'ngAfterContentChecked->',
+      this.projectContentHeader.nativeElement.innerHTML
+    );
+  }
+
+  ngAfterViewInit() {
+    //Header is available here
+    console.log(
+      'ProjectContent2->',
+      'ngAfterViewInit->',
+      this.projectContentHeader.nativeElement.innerHTML
+    );
+  }
+
+  ngAfterViewChecked() {
+    //Header is available here
+    console.log(
+      'ProjectContent2->',
+      'ngAfterViewChecked->',
+      this.projectContentHeader.nativeElement.innerHTML
+    );
   }
 }
