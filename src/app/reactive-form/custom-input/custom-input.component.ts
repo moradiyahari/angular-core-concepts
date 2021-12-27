6a2136420dc35dd4a3f9e3446794365894156e16
moradiyahari <moradiyahari@ymail.com>
2021-12-27 21:47:52 +0530

    3 files modified

--- src/app/reactive-form/custom-input/custom-input.component.ts
+++ src/app/reactive-form/custom-input/custom-input.component.ts
@@ -1,16 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'custom-input',
   templateUrl: './custom-input.component.html'
 })
 export class CustomInputComponent {
-  @Input() id: string;
-  @Input() label: string;
-  @Input() type: string;
   @Input() controlName: string;
-
-  @Input() form!: FormGroup;
+  @Input() form!: any;
 }
 
