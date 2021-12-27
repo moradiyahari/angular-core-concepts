44693f0565d0ca31d87896cff516f00f991b997e
moradiyahari <moradiyahari@ymail.com>
2021-12-27 23:09:32 +0530

    2 files modified

--- src/app/reactive-form/custom-input/custom-input.component.ts
+++ src/app/reactive-form/custom-input/custom-input.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'custom-input',
-  templateUrl: './custom-input.component.html'
+  templateUrl: './custom-input.component.html',
+  styles:[`label{ width: 100px;}`]
 })
 export class CustomInputComponent {
   @Input() form: any;
--- src/app/reactive-form/custom-list/custom-list.component.ts
+++ src/app/reactive-form/custom-list/custom-list.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'custom-list',
   templateUrl: './custom-list.component.html',
+  styles:[`label{ width: 100px;}`]
 })
 export class CustomListComponent {
   @Input() form: any;
